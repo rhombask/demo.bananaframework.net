521e96451e5cb512f0b0db86691504d1ebbb4e8f
rhombask <jmson@infinihance.com>
2014-09-25 17:34:59 +0900

    로그인 페이지 추가

---
+++ Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="demo.bananaframework.net.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title>로그인</title>
+	<link href="/Common/Css/default.css" rel="stylesheet" />
+	<link href="/Common/Css/login.css" rel="stylesheet" />
+	<script src="/Common/Scripts/jquery-1.11.1.min.js" type="text/javascript"></script>
+	<script src="/Framework/Common/Common.js" type="text/javascript"></script>
+</head>
+<body>
+	<form id="form1" runat="server">
+	<h1>Log In</h1>
+	<fieldset id="inputs">
+		<legend>로그인 항목</legend>
+		<asp:TextBox ID="_txtUserID" runat="server" CssClass="loginID" placeholder="로그인 계정" autofocus="autofocus" required="required" />
+		<asp:TextBox ID="_txtUserPassword" runat="server" TextMode="Password" CssClass="loginPW" placeholder="비밀번호" required="required" />
+	</fieldset>
+	<fieldset id="actions">
+		<legend>로그인 버튼</legend>
+		<asp:Button ID="_btnLOGIN" runat="server" Text="로그인" CssClass="loginButton" OnClick="_btnLOGIN_Click" />
+		<a href="javascript:findIdAndPW();">아이디 혹은 비밀번호를 잊으셨나요?</a>
+	</fieldset>
+	</form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace demo.bananaframework.net
+{
+	/// <summary>
+	/// 로그인 페이지입니다.
+	/// </summary>
+	public partial class Default : Control.BasePage
+	{
+        #region Page_Load : 페이지 로드
+        /// <summary>
+        /// 페이지 로드
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!IsPostBack)
+			{
+			}
+        }
+        #endregion
+
+		#region _btnLOGIN_Click : 로그인버튼 클릭
+		/// <summary>
+		/// 로그인버튼 클릭
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		protected void _btnLOGIN_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				// 로그인
+                DataTable _dt = base.GetDataTable("WSP_LOGIN"
+                    , _txtUserID.Text.Trim()
+                    );
+
+                if (base.GetDecryptTripleDES(_dt.Rows[0]["PWD"].ToString()) != _txtUserPassword.Text.Trim())
+                {
+					BANANA.Web.NotificationBar.Show("비밀번호가 틀립니다.", BANANA.Web.NotificationBar.NotificationType.Error);
+                    return;
+                }
+
+                base.SetCookie("UserID", _txtUserID.Text.Trim());
+                base.SetCookie("UserName", _dt.Rows[0]["NAME"].ToString());
+                base.SetCookie("UserGroupName", _dt.Rows[0]["USER_GRP"].ToString());
+                base.SetCookie("UserGroupCode", _dt.Rows[0]["USER_GRP_CD"].ToString());
+                Response.Redirect("/Framework/Banana/Default.aspx", false);
+			}
+			catch (Exception err)
+			{
+				base.SetCookie("UserID", "");
+				base.SetCookie("UserName", "");
+				base.SetCookie("UserGroupName", "");
+                base.SetCookie("UserGroupCode", "");
+
+                BANANA.Web.NotificationBar.Show(err.Message, BANANA.Web.NotificationBar.NotificationType.Error);
+			}
+		}
+		#endregion
+	}
+}
