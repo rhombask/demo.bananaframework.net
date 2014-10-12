eac3eabf6031992d68badc15bfed3de6e113f230
rhombask <jmson@infinihance.com>
2014-10-12 22:41:22 +0900

    이용자 로그인을 위해서, Xenon 프레임워크 추가 중...

---
+++ Common/Scripts/devexpress-web-14.1/layouts/Desktop/DesktopLayout.js
@@ -0,0 +1,11 @@
+﻿(function($, DX, undefined) {
+    var layoutSets = DX.framework.aspx.layoutSets;
+    layoutSets["desktop"] = layoutSets["desktop"] || [];
+    layoutSets["desktop"].push({
+        platform: "generic",
+        controller: new DX.framework.aspx.DefaultLayoutController({
+            name: "desktop",
+            disableViewLoadingState: true
+        })
+    })
+})(jQuery, DevExpress);
