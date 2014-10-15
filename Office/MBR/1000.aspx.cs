dd59febea40765334e63e725585ee6a138d1d79e
rhombask <jmson@infinihance.com>
2014-10-15 14:58:13 +0900

    일반 회원용 마이오피스 개발 중

---
+++ Office/MBR/1000.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace demo.bananaframework.net.Office.MBR
+{
+	public partial class _1000 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+		}
+	}
+}
