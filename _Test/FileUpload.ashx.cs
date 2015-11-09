db3af1b889ff25cbdf0f6095402447956c4e4d03
Joshua Son <rhombask@gmail.com>
2015-11-09 20:57:43 +0900

    파일 업로드 구현 중

---
+++ _Test/FileUpload.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demo.bananaframework.net._Test
+{
+	/// <summary>
+	/// FileUpload1의 요약 설명입니다.
+	/// </summary>
+	public class FileUpload1 : IHttpHandler
+	{
+
+		public void ProcessRequest(HttpContext context)
+		{
+			context.Response.ContentType = "text/plain";
+			context.Response.Write("Hello World");
+		}
+
+		public bool IsReusable
+		{
+			get
+			{
+				return false;
+			}
+		}
+	}
+}
